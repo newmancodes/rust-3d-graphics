ebadabf06ff8c7b3aba2d27792f4f5a293f19a19
steve <steve@newman.digital>
2021-06-14 22:11:53 +0100

    Part 1 Complete.

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
 const rust = import('./pkg/rust_3d_graphics');
+const canvas = document.getElementById('canvas');
+const gl = canvas.getContext('webgl', { antialias: true });
 
-rust.then(m => m.say_hello_from_rust())
-    .catch(console.error);
+rust.then(m => {
+    if (!gl) {
+        alert('Failed to initialise WebGL');
+        return;
+    }
+
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+    const FPS_THROTTLE = 1000.0 / 30.0;
+    const client = new m.Client();
+    const initialTime = Date.UTC;
+    var lastDrawTime = -1;
+
+    function render() {
+        window.requestAnimationFrame(render);
+        const currentTime = Date.UTC;
+        
+        if (currentTime >= lastDrawTime + FPS_THROTTLE) {
+            lastDrawTime = currentTime;
+
+            if (window.innerHeight != canvas.height || window.innerWidth != canvas.width) {
+                canvas.height = window.innerHeight;
+                canvas.clientHeight = window.innerHeight;
+                canvas.style.height = window;innerHeight;
+
+                canvas.width = window.innerWidth;
+                canvas.clientWidth = window.innerWidth;
+                canvas.style.width = window;innerWidths;
+
+                gl.viewport(0, 0, window.innerHeight, window.innerWidth);
+            }
+        }
+
+        let elapsedTime = currentTime - initialTime;
+        client.update(elapsedTime, window.innerHeight, window.innerWidth);
+        client.render();
+    }
+
+    render();
+});
--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,21 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn say_hello_from_rust() {
-    log("Hello, World!... from rust");
+pub struct Client {
+}
+
+#[wasm_bindgen]
+impl Client {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        Self {
+        }
+    }
+
+    pub fn update(&mut self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {
+        Ok(())
+    }
+
+    pub fn render(&self) {
+    }
 }
